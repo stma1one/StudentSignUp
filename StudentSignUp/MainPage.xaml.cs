1ff0437521eb7d738bb693847825f14d85d5eba4
Home <Home@LAPTOP-2E6V6ACM>
2021-03-07 21:15:21 +0200

    Add project files.

---
+++ StudentSignUp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StudentSignUp
+{
+    public partial class MainPage : ContentPage
+    {
+        Student Sd;
+        public MainPage()
+        {
+            Sd = new Student();
+            InitializeComponent();
+            BindingContext = Sd;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Page p = new RegistrationP2();
+            p.BindingContext = this.BindingContext;
+            await Navigation.PushAsync(p);
+        }
+    }
+}
---
+++ StudentSignUp/RegistrationP2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StudentSignUp
+{
+    public partial class RegistrationP2 : ContentPage
+    {
+       
+        public RegistrationP2()
+        {
+           
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Page p = new RegistrationP3();
+            p.BindingContext = this.BindingContext;
+            await Navigation.PushAsync(p);
+        }
+    }
+}
